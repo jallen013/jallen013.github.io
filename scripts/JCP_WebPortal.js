bf82e6529ba7a779d1194fff4fa2c76411379057
jallen013 <jallen013@gmail.com>
2018-09-23 14:57:19 -0500

    Add files via upload

--- scripts/JCP_WebPortal.js
+++ scripts/JCP_WebPortal.js
@@ -1,34 +1,41 @@
 
-window.addEventListener("load", function(){
-    if (window.matchMedia("(min-width: 700px)").matches) 		// WIDE
-    {
-		document.getElementById("shield").style.top = "8%";
-        document.getElementById("wideTable").style.display = "block";
-        document.getElementById("narrowTable").style.display = "none";
+function wide(){
+    document.getElementById("wideTable").style.display = "block";
+    document.getElementById("narrowTable").style.display = "none";
+}
 
-    } 		
-    else														// NARROW
+function narrow(){
+    document.getElementById("wideTable").style.display = "none";
+    document.getElementById("narrowTable").style.display = "block";
+}
+
+
+window.addEventListener("load", function(){
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        narrow();
+    }
+    else
     {
-		document.getElementById("shield").style.top = "8%";
-        document.getElementById("wideTable").style.display = "none";
-        document.getElementById("narrowTable").style.display = "block";
+        if (window.matchMedia("(min-width: 750px)").matches) 		// WIDE
+        {
+    		wide();
+        } 		
+        else														// NARROW
+        {
+    		narrow();
+        }
     }
 });
 
 
 window.addEventListener("resize", function(){					
-    if (window.matchMedia("(min-width: 700px)").matches) 		// WIDE
+    if (window.matchMedia("(min-width: 750px)").matches) 		// WIDE
     {
-    	document.getElementById("shield").style.top = "8%";
-        document.getElementById("wideTable").style.display = "block";
-        document.getElementById("narrowTable").style.display = "none";
-
+        wide();
     } 		
     else														// NARROW
     {
-		document.getElementById("shield").style.top = "8%";
-        document.getElementById("wideTable").style.display = "none";
-        document.getElementById("narrowTable").style.display = "block";
+		narrow();
     }
 });
 
